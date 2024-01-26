3e57689afc981158076644aeee05491199e61847
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-01-26 10:38:43 +0100

    drop json

--- chesscom.sh
+++ chesscom.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-true>chesscom.json
 true>chesscom.pgn
 
 archives=$(curl https://api.chess.com/pub/player/pnmadelaine/games/archives | jq -r ".archives[]")
@@ -9,6 +8,5 @@ archives=$(curl https://api.chess.com/pub/player/pnmadelaine/games/archives | jq
 for url in $archives
 do
     json=$(curl "$url")
-    echo "$json" | jq >> chesscom.json
-    echo "$json" | jq -r ".games[].pgn" >> chesscom.pgn
+    echo "$json" | jq -r '.games[].pgn' >> chesscom.pgn
 done
--- lichess.sh
+++ lichess.sh
@@ -3,6 +3,4 @@ set -euo pipefail
 
 curl "https://lichess.org/api/games/user/pnmadelaine?clocks=true&pgnInJson=true&sort=dateAsc" \
 	-H "Accept: application/x-ndjson" \
-	| jq > lichess.json
-
-jq ".pgn" -r < lichess.json > lichess.pgn
+	| jq '.pgn' -r > lichess.pgn
