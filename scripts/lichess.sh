bed756a7ca72cc2e70ea04d9abcf5925c99511fe
Paul-Nicolas Madelaine <pnm@pnm.tf>
2024-03-05 18:14:15 +0100

    download otb games from lichess

--- scripts/lichess.sh
+++ scripts/lichess.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 curl "https://lichess.org/api/games/user/pnmadelaine?clocks=true&pgnInJson=true&sort=dateAsc" \
 	-H "Accept: application/x-ndjson" \
 	| jq '.pgn' -r > lichess.pgn
+
+curl "https://lichess.org/api/study/65ikUc0b.pgn" > otb.pgn
